449a9f327109b7cf2f3976fb00ebcd138b7d1c6e
abinocoe <abigailcoe@gmail.com>
2016-11-08 12:08:53 +0000

    js hamming solution one

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,18 @@
+module.exports = Hamming;
+
+function Hamming() {
+}
+
+Hamming.prototype.compute = function(stringOne, stringTwo) {
+  this.count = 0;
+  if (stringOne.length === stringTwo.length) {
+    for (var i = 0; i <= stringOne.length; i++) {
+      if (stringOne[i] !== stringTwo[i]) {
+        this.count ++;
+      }
+    }
+  } else {
+    throw 'DNA strands must be of equal length.';
+  }
+  return this.count;
+};
