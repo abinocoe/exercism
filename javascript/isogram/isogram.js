b5e3f1a040f2a032ed1e8003f5bc23f35a68034d
abinocoe <abigailcoe@gmail.com>
2016-12-24 12:17:10 +0000

    js isogram solution two

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -3,24 +3,17 @@ const Isogram = function(input) {
 };
 
 Isogram.prototype.isIsogram = function() {
-  let hi = this.testCase;
+  let string = this.testCase;
 
-  while (hi.length > 1) {
-    hi = hi.toLowerCase();
-    let letter = hi.slice(0, 1);
-    hi = hi.slice(1);
-    if (hi.indexOf(letter) !== -1 && letter !== '-' && letter !== ' ') {
-      return false;
-    }
+  while (string.length > 1) {
+    string     = string.toLowerCase()
+                       .replace(/[\s\-]/g, '');
+    let letter = string.slice(0, 1);
+    string     = string.slice(1);
+    
+    if (string.match(letter)) return false;
   }
-
-  if (hi.length === 1) {
-    return true;
-  }
-
-  // DESTROY THE INPUT WORD BY SLICING LETTERS OFF AND THEN TESTING THOSE
-  // look at while and unless - basically you need to break the loop
-
+  return true;
 };
 
 module.exports = Isogram;
