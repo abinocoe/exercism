bdc3cb7e7fc759dc3635e282a85b214df0bde588
Abigail Coe <abigailcoe@gmail.com>
2020-09-23 09:31:46 +0100

    rust armstrong numbers v2

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,15 +1,9 @@
-use std::convert::TryInto;
-
 pub fn is_armstrong_number(num: u32) -> bool {
     let number_string = num.to_string();
-    let length_of_number_string = number_string.len();
+    let length_of_number_string = number_string.len() as u32;
     let each_digit_powered: u32 = number_string
         .chars()
-        .map(|d| {
-            d.to_digit(10)
-                .unwrap()
-                .pow(length_of_number_string.try_into().unwrap())
-        })
+        .map(|d| d.to_digit(10).unwrap().pow(length_of_number_string))
         .sum();
 
     each_digit_powered == num
